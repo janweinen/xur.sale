0d7c1ac14a89ed049b859c8f8e735fb0ef1c95ae
Jan Weinen <30630373+janweinen@users.noreply.github.com>
2019-07-02 10:33:21 +0200

    added react-ga

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Layout from "./components/Layout";
+import ReactGA from "react-ga";
 
 import "./styles.css";
+
+ReactGA.initialize("UA-143175874-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<Layout />, rootElement);
