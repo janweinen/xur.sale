feb5c951420a6a19769b690fd36fedbd22ed07a8
Jan Weinen <30630373+janweinen@users.noreply.github.com>
2019-07-03 09:55:05 +0200

    updated location

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import InventoryList from "./InventoryList";
 import { Location } from "./Helpers";
 
-const planet = "tower";
+const planet = "";
 
 class Footer extends Component {
   render() {
