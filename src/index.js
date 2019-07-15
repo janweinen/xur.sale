2cec90a9dc36aebe46a70624eb90443c1a7104e1
Jan Weinen <30630373+janweinen@users.noreply.github.com>
2019-07-15 15:44:46 +0200

    outsourced svg background

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import ReactDOM from "react-dom";
 import ReactGA from "react-ga";
 
 import "./styles.css";
+import "./backgroundPattern.css";
 
 ReactGA.initialize("UA-143175874-1");
 ReactGA.pageview(window.location.pathname + window.location.search);
