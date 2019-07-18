f36fc8aa2f40d3a0dc11860cc843d885d30d82d5
Jan Weinen <30630373+janweinen@users.noreply.github.com>
2019-07-18 12:06:04 +0200

    authentication prep

--- src/components/Globals.jsx
+++ src/components/Globals.jsx
@@ -2,9 +2,11 @@ export const Globals = {
   key: {
     bungie: process.env.REACT_APP_BUNGIE_API_KEY
   },
+  client_id: 23891,
   url: {
     bungie: "https://www.bungie.net",
-    trn: "https://db.destinytracker.com/d2/en/items/"
+    trn: "https://db.destinytracker.com/d2/en/items/",
+    oauth: "https://www.bungie.net/Platform/App/OAuth/Token/"
   }
 };
 
